e04fd2596fc5292366999521e1df0025edb19386
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-07-18 17:25:27 +0200

    :bug: Music

--- src/components/Music.js
+++ src/components/Music.js
@@ -6,12 +6,13 @@ export default class Music extends Component {
     return (
       <iframe
         allow="autoplay"
-        height="0"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/231352258&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
         className={css(`
           position: absolute;
-          visibility: hidden;
+          z-index: -1;
         `)}
+        height="0"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/231352258&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        title="Background Music"
         width="0"
       ></iframe>
     );
