0cdc066366ba0bc33f0019212920333f2278c095
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-07-19 23:16:37 +0200

    Revert ":pencil2: About text"
    This reverts commit e9e64c17dc08ae4623368de50c260630689e86bf.

--- src/components/About.js
+++ src/components/About.js
@@ -54,7 +54,7 @@ export default class App extends Component {
           >
             <p>You have to be careful what you post online these days. Isn't that boring? That's why Feestbook is here. This is a place where you can do whatever you want, without having to worry about privacy... even when you are drunk!</p>
 
-            <p>Our most important values are freedom, privacy and fun. You are completely anonymous and <strike>everything you post here will be deleted within 100 seconds</strike> (still working on that). There is no censorship and no shame here. Only wonderful people who like to have fun with others.</p>
+            <p>Our most important values are freedom, privacy and fun. You are completely anonymous and everything you post here will be deleted within 100 seconds. There is no censorship and no shame here. Only wonderful people who like to have fun with others.</p>
 
             <p>The word "Feest" is dutch for party, so don't be shy and let's party hard! Don't worry... everything will be forgotten tomorrow.</p>
           </div>
