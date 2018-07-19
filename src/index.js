8fc27a22103cc150ab63f4fff61ac3cb2085173c
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-07-19 21:20:10 +0200

    :lock: Store firebase config in ignored env file

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import App from './components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import env from './env.js';
 
-firebase.initializeApp({
-  apiKey: "AIzaSyB9mcJm0lcxdrSkl3LjCjYhX7i6qEo_TRM",
-  authDomain: "feestbook-e645f.firebaseapp.com",
-  databaseURL: "https://feestbook-e645f.firebaseio.com",
-  projectId: "feestbook-e645f",
-  storageBucket: "feestbook-e645f.appspot.com",
-  messagingSenderId: "712269568004"
-});
+firebase.initializeApp(env.firebase);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
