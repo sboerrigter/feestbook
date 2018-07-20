2b2cf29a8ffa0bf26b628509d273a5649feefa16
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-07-20 10:43:46 +0200

    :pencil2: Delete time

--- src/components/About.js
+++ src/components/About.js
@@ -54,7 +54,7 @@ export default class App extends Component {
           >
             <p>You have to be careful what you post online these days. Isn't that boring? That's why Feestbook is here. This is a place where you can do whatever you want, without having to worry about privacy... even when you are drunk!</p>
 
-            <p>Our most important values are freedom, privacy and fun. You are completely anonymous and everything you post here will be deleted within 100 seconds. There is no censorship and no shame here. Only wonderful people who like to have fun with others.</p>
+            <p>Our most important values are freedom, privacy and fun. You are completely anonymous and everything you post here will be deleted after 10 minutes. There is no censorship and no shame here. Only wonderful people who like to have fun with others.</p>
 
             <p>The word "feest" is dutch for party, so don't be shy and let's party hard! Don't worry... everything will be forgotten tomorrow.</p>
           </div>
